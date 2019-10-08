df12e562ef1b07c944f5131a0ea9c242fbe48f4c
YuHeng Shi <yuhengshi@ucsb.edu>
2019-10-08 13:45:28 -0700

    added the line

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
     
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }
+    String html = "<h1>Hello World!</h1>\n" +
+    "<p>This web app is powered by \n" +
+    "<a href='https://github.com/yuhengshi/spring-boot-minimal-webapp'>this github repo</a></p>\n";
+return html;
     
 }
